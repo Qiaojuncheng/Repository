2acd17654e588efd0af7cf02f149a4c8cf973265
Qiaojuncheng <huangyawei992@sina.com>
2018-07-13 16:48:25 +0800

    提交新的项目

---
+++ singdemo/singdemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  singdemo
+//
+//  Created by MYMAc on 2018/7/13.
+//  Copyright © 2018年 ShangYu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ singdemo/singdemo/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  singdemo
+//
+//  Created by MYMAc on 2018/7/13.
+//  Copyright © 2018年 ShangYu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
